e49cce780dda9620e4ac268a2b5dd8b6f1d1da8f
Konstantin Nered <konstantin.n616@gmail.com>
2025-07-22 21:43:02 +0700

    created template for comparison body

--- src/Components/CompareMain.tsx
+++ src/Components/CompareMain.tsx
@@ -1,37 +1,86 @@
+import type { modelType } from '../data/phoneModels';
+import { models, keyLabels } from '../data/phoneModels';
+
+import { IoIosCheckmarkCircle, IoIosCloseCircle } from 'react-icons/io';
+
 export default function CompareMain() {
+    const modelKeys = Object.keys(models[0]).filter(
+        (key) => key !== 'id'
+    ) as (keyof Omit<modelType, 'id'>)[];
+
+    const renderValue = (
+        key: keyof Omit<modelType, 'id'>,
+        value: modelType[keyof modelType]
+    ) => {
+        if (typeof value === 'boolean') {
+            return value ? (
+                <div className='flex h-full w-full items-center'>
+                    <IoIosCheckmarkCircle className='text-green-600 text-2xl' />
+                </div>
+            ) : (
+                <div className='flex h-full w-full items-center'>
+                    <IoIosCloseCircle className='text-red-600 text-2xl' />
+                </div>
+            );
+        }
+        if (key === 'price') {
+            return `${value.toLocaleString('fr-fr')} ₽`;
+        }
+        return value;
+    };
+
     return (
-        <main className='bg-bg'>
-            {/* compare table */}
-            <div className='wrapper'>
-                {/* properties of models */}
-                <div className="w-full max-w-1/5 flex flex-col">
-                    <div className="border-y-[1px] border-y-secondary/35 py-4">Производитель</div>
-                    <div className="border-b-[1px] border-b-secondary/35 py-4">Год релиза</div>
-                    <div className="border-b-[1px] border-b-secondary/35 py-4">Диагноль экрана (Дюйм)</div>
-                    <div className="border-b-[1px] border-b-secondary/35 py-4">Страна-производитель</div>
-                    <div className="border-b-[1px] border-b-secondary/35 py-4">Объем памяти</div>
+        <main className='bg-bg pb-12 px-2'>
+            <div className='wrapper overflow-x-auto pb-1.5 scrollbar scrollbar-h-2.5 scrollbar-thumb-secondary-light/50 scrollbar-thumb-rounded-xl scrollbar-hover:scrollbar-thumb-secondary-light/75'>
+                <div className='w-full flex flex-col'>
+                    {modelKeys.map(
+                        (key, index) =>
+                            index !== 0 && (
+                                <div key={key} className='w-full flex'>
+                                    {index === 1 ? (
+                                        <div className='w-full min-w-[calc(25%+1rem)] flex border-t-[1px] border-b-[1px] pr-4 border-secondary/35 py-6'>
+                                            <div className='text-secondary-light uppercase font-medium'>
+                                                {keyLabels[key]}
+                                            </div>
+                                        </div>
+                                    ) : (
+                                        <div className='w-full min-w-[calc(25%+1rem)] flex border-b-[1px] pr-4 border-secondary/35 py-6'>
+                                            <div className='text-secondary-light uppercase font-medium'>
+                                                {keyLabels[key]}
+                                            </div>
+                                        </div>
+                                    )}
+                                    {models.slice(0, 4).map((model) =>
+                                        key === 'brand' ? (
+                                            <div
+                                                key={model.id}
+                                                className='w-full min-w-[calc(25%+1rem)] flex border-t-[1px] border-b-[1px] pr-4 border-secondary/35 py-6'
+                                            >
+                                                <div className='text-secondary capitalize font-medium'>
+                                                    {renderValue(
+                                                        key,
+                                                        model[key]
+                                                    )}
+                                                </div>
+                                            </div>
+                                        ) : (
+                                            <div
+                                                key={model.id}
+                                                className='w-full min-w-[calc(25%+1rem)] flex border-b-[1px] pr-4 border-secondary/35 py-6'
+                                            >
+                                                <div className='text-secondary capitalize font-medium'>
+                                                    {renderValue(
+                                                        key,
+                                                        model[key]
+                                                    )}
+                                                </div>
+                                            </div>
+                                        )
+                                    )}
+                                </div>
+                            )
+                    )}
                 </div>
-                <div className="w-full flex flex-col">
-                    <div className="border-y-[1px] border-y-secondary/35 py-4">Apple</div>
-                    <div className="border-b-[1px] border-b-secondary/35 py-4">2022</div>
-                    <div className="border-b-[1px] border-b-secondary/35 py-4">6.1</div>
-                    <div className="border-b-[1px] border-b-secondary/35 py-4">Китай</div>
-                    <div className="border-b-[1px] border-b-secondary/35 py-4">128 гб.</div>
-                </div>
-                <div className="w-full flex flex-col">
-                    <div className="border-y-[1px] border-y-secondary/35 py-4">Apple</div>
-                    <div className="border-b-[1px] border-b-secondary/35 py-4">2022</div>
-                    <div className="border-b-[1px] border-b-secondary/35 py-4">6.1</div>
-                    <div className="border-b-[1px] border-b-secondary/35 py-4">Китай</div>
-                    <div className="border-b-[1px] border-b-secondary/35 py-4">128 гб.</div>
-                </div>
-                {/* <div className="w-full flex flex-col">
-                    <div className="border-y-[1px] border-y-secondary/35 py-4">Apple</div>
-                    <div className="border-b-[1px] border-b-secondary/35 py-4">2022</div>
-                    <div className="border-b-[1px] border-b-secondary/35 py-4">6.1</div>
-                    <div className="border-b-[1px] border-b-secondary/35 py-4">Китай</div>
-                    <div className="border-b-[1px] border-b-secondary/35 py-4">128 гб.</div>
-                </div> */}
             </div>
         </main>
     );
