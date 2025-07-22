73e2b6d320eadad7b47a34d4bea0697c6ccac9ab
Konstantin Nered <konstantin.n616@gmail.com>
2025-07-22 18:05:37 +0700

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+import NavBar from './Components/NavBar';
+import CompareBlock from './Components/CompareBlock';
+
+export default function App(){
+    return (
+        <div className='container max-w-[1440px] mx-auto'>
+            <NavBar />
+            <CompareBlock />
+        </div>
+    )
+}
