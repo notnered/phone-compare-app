6d4bd8111a89acac912bdf27fb1a1d632a912a1b
Konstantin Nered <konstantin.n616@gmail.com>
2025-07-23 00:09:20 +0700

    did 90% of header

---
+++ src/Components/CompareFilters.tsx
@@ -0,0 +1,21 @@
+export default function CompareFilters() {
+    return (
+        <div className='wrapper w-full flex items-center justify-between'>
+            <div>
+                <h1 className='text-5xl text-secondary-light font-medium'>
+                    Смартфоны
+                </h1>
+            </div>
+            <div className='flex gap-x-1.5 text-primary'>
+                Отобразить товары:
+                <div className='hover:cursor-pointer'>2</div>
+                <div className='hover:cursor-pointer underline underline-offset-4'>
+                    3
+                </div>
+                <div className='hover:cursor-pointer'>4</div>
+                <div className='hover:cursor-pointer'>5</div>
+                <div className='hover:cursor-pointer'>6</div>
+            </div>
+        </div>
+    );
+}
