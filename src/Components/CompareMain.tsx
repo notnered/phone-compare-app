7185eca165daa87cbb01c487a25155fbf2ed7b1e
Konstantin Nered <konstantin.n616@gmail.com>
2025-07-23 00:09:49 +0700

    simplified main component

--- src/Components/CompareMain.tsx
+++ src/Components/CompareMain.tsx
@@ -1,12 +1,14 @@
 import type { modelType } from '../data/phoneModels';
 import { models, keyLabels } from '../data/phoneModels';
-
 import { IoIosCheckmarkCircle, IoIosCloseCircle } from 'react-icons/io';
+import CompareHeader from './CompareHeader';
 
 export default function CompareMain() {
-    const modelKeys = Object.keys(models[0]).filter(
-        (key) => key !== 'id'
-    ) as (keyof Omit<modelType, 'id'>)[];
+    const modelKeys = Object.keys(keyLabels) as (keyof typeof keyLabels)[];
+
+    const modelsVisual = models.map(({ id, name, pic }) => ({ id, name, pic }));
+
+    const modelsSpecs = models.map(({ id, ...rest }) => ({ id, ...rest }));
 
     const renderValue = (
         key: keyof Omit<modelType, 'id'>,
@@ -23,63 +25,41 @@ export default function CompareMain() {
                 </div>
             );
         }
-        if (key === 'price') {
-            return `${value.toLocaleString('fr-fr')} ₽`;
-        }
+        if (key === 'memory_capacity') return `${value} Гб`;
+        if (key === 'refresh_rate') return `${value} Гц`;
+        if (key === 'price') return `${value.toLocaleString('fr-fr')} ₽`;
         return value;
     };
 
     return (
         <main className='bg-bg pb-12 px-2'>
-            <div className='wrapper overflow-x-auto pb-1.5 scrollbar scrollbar-h-2.5 scrollbar-thumb-secondary-light/50 scrollbar-thumb-rounded-xl scrollbar-hover:scrollbar-thumb-secondary-light/75'>
+            <div className='wrapper flex flex-col overflow-x-auto pb-1.5 scrollbar scrollbar-h-2.5 scrollbar-thumb-secondary-light/50 scrollbar-thumb-rounded-xl scrollbar-hover:scrollbar-thumb-secondary-light/75'>
                 <div className='w-full flex flex-col'>
-                    {modelKeys.map(
-                        (key, index) =>
-                            index !== 0 && (
-                                <div key={key} className='w-full flex'>
-                                    {index === 1 ? (
-                                        <div className='w-full min-w-[calc(25%+1rem)] flex border-t-[1px] border-b-[1px] pr-4 border-secondary/35 py-6'>
-                                            <div className='text-secondary-light uppercase font-medium'>
-                                                {keyLabels[key]}
-                                            </div>
-                                        </div>
-                                    ) : (
-                                        <div className='w-full min-w-[calc(25%+1rem)] flex border-b-[1px] pr-4 border-secondary/35 py-6'>
-                                            <div className='text-secondary-light uppercase font-medium'>
-                                                {keyLabels[key]}
-                                            </div>
-                                        </div>
-                                    )}
-                                    {models.slice(0, 4).map((model) =>
-                                        key === 'brand' ? (
-                                            <div
-                                                key={model.id}
-                                                className='w-full min-w-[calc(25%+1rem)] flex border-t-[1px] border-b-[1px] pr-4 border-secondary/35 py-6'
-                                            >
-                                                <div className='text-secondary capitalize font-medium'>
-                                                    {renderValue(
-                                                        key,
-                                                        model[key]
-                                                    )}
-                                                </div>
-                                            </div>
-                                        ) : (
-                                            <div
-                                                key={model.id}
-                                                className='w-full min-w-[calc(25%+1rem)] flex border-b-[1px] pr-4 border-secondary/35 py-6'
-                                            >
-                                                <div className='text-secondary capitalize font-medium'>
-                                                    {renderValue(
-                                                        key,
-                                                        model[key]
-                                                    )}
-                                                </div>
-                                            </div>
-                                        )
-                                    )}
+                    {modelKeys.map((key, index) => (
+                        <div key={key} className='w-full flex'>
+                            <div
+                                className={`w-full min-w-[calc(25%+1rem)] flex pr-4 py-6 ${
+                                    index === 0 ? 'border-t-[1px]' : ''
+                                } border-b-[1px] border-secondary/35`}
+                            >
+                                <div className='text-secondary-light uppercase font-medium'>
+                                    {keyLabels[key]}
                                 </div>
-                            )
-                    )}
+                            </div>
+                            {modelsSpecs.map((model) => (
+                                <div
+                                    key={model.id + key}
+                                    className={`w-full min-w-[calc(25%+1rem)] flex pr-4 py-6 ${
+                                        index === 0 ? 'border-t-[1px]' : ''
+                                    } border-b-[1px] border-secondary/35`}
+                                >
+                                    <div className='text-secondary capitalize font-medium'>
+                                        {renderValue(key, model[key])}
+                                    </div>
+                                </div>
+                            ))}
+                        </div>
+                    ))}
                 </div>
             </div>
         </main>
