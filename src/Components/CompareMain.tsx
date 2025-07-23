d6d2b544844f2fed2c68df65b182d0af940e0405
Konstantin Nered <konstantin.n616@gmail.com>
2025-07-24 00:07:01 +0700

    added difference checker and updated logic with redux

--- src/Components/CompareMain.tsx
+++ src/Components/CompareMain.tsx
@@ -2,15 +2,23 @@ import type { modelType } from '../data/phoneModels';
 import { models, keyLabels } from '../data/phoneModels';
 import { IoIosCheckmarkCircle, IoIosCloseCircle } from 'react-icons/io';
 import CompareModels from './CompareModels';
+import { useDispatch, useSelector } from 'react-redux';
+import type { RootState } from '../store/store';
+import { useEffect } from 'react';
+import { setModels } from '../store/compareSlice';
 
 export default function CompareMain({ limit }: { limit: number }) {
     const modelKeys = Object.keys(keyLabels) as (keyof typeof keyLabels)[];
 
-    const limitedModels = models.slice(0, limit);
+    const selectedModels = useSelector(
+        (state: RootState) => state.compare.selectedModels
+    );
 
-    const modelsVisual = limitedModels.map(({ id, name, pic }) => ({ id, name, pic }));
+    const dispatch = useDispatch();
 
-    const modelsSpecs = limitedModels.map(({ id, ...rest }) => ({ id, ...rest }));
+    useEffect(() => {
+        dispatch(setModels(models.slice(0, limit)));
+    }, [dispatch, limit]);
 
     const renderValue = (
         key: keyof Omit<modelType, 'id'>,
@@ -33,44 +41,70 @@ export default function CompareMain({ limit }: { limit: number }) {
         return value;
     };
 
+    const getKeysWithDifferences = (
+        models: modelType[],
+        keys: (keyof typeof keyLabels)[]
+    ): (keyof typeof keyLabels)[] => {
+        return keys.filter((key) => {
+            const firstValue = models[0][key];
+            return models.some((model) => model[key] !== firstValue);
+        });
+    };
+
+    const showDifferences = useSelector(
+        (state: RootState) => state.compare.showDifferences
+    );
+
+    const visibleKeys = showDifferences
+        ? getKeysWithDifferences(selectedModels, modelKeys)
+        : modelKeys;
+
+    console.log(visibleKeys);
+
     return (
         <main className='pb-12'>
             <div
-                className={`flex flex-col ${modelsSpecs.length > 3 ? 'overflow-x-auto' : 'overflow-x-hidden'} wrapper px-2 pb-2.5 scrollbar scrollbar-h-2.5 scrollbar-thumb-secondary-light/50 scrollbar-thumb-rounded-xl scrollbar-hover:scrollbar-thumb-secondary-light/75`}
+                className={`flex flex-col ${
+                    selectedModels.length > 3
+                        ? 'overflow-x-auto'
+                        : 'overflow-x-hidden'
+                } wrapper px-2 pb-2.5 scrollbar scrollbar-h-2.5 scrollbar-thumb-secondary-light/50 scrollbar-thumb-rounded-xl scrollbar-hover:scrollbar-thumb-secondary-light/75`}
                 style={{
                     background:
                         'linear-gradient(to bottom, #ffffff 0%, #ffffff 18rem, rgba(244, 249, 252, 1) 18rem, rgba(244, 249, 252, 1) 100%)',
                 }}
             >
-                <div className=''>
-                    <div className='flex flex-col w-full'>
-                        <CompareModels models={modelsVisual} />
-                        {modelKeys.map((key, index) => (
-                            <div key={key} className='flex'>
+                <div className='flex flex-col w-full'>
+                    <CompareModels models={selectedModels} />
+                    {visibleKeys.length === 0 && 
+                        <div className='text-center pt-4 pb-2 text-xl'>
+                            Нет различий
+                        </div>}
+                    {visibleKeys.map((key, index) => (
+                        <div key={key} className='flex'>
+                            <div
+                                className={`min-w-[calc(25%+0.5rem)] w-full flex pr-4 py-6 ${
+                                    index === 0 ? 'border-t-[1px]' : ''
+                                } border-b-[1px] border-secondary/35`}
+                            >
+                                <div className='text-secondary-light uppercase font-medium'>
+                                    {keyLabels[key]}
+                                </div>
+                            </div>
+                            {selectedModels.map((model, ind) => (
                                 <div
+                                    key={`${model.id}-${key}-${ind}`}
                                     className={`min-w-[calc(25%+0.5rem)] w-full flex pr-4 py-6 ${
                                         index === 0 ? 'border-t-[1px]' : ''
                                     } border-b-[1px] border-secondary/35`}
                                 >
-                                    <div className='text-secondary-light uppercase font-medium'>
-                                        {keyLabels[key]}
+                                    <div className='text-secondary capitalize font-medium'>
+                                        {renderValue(key, model[key])}
                                     </div>
                                 </div>
-                                {modelsSpecs.map((model) => (
-                                    <div
-                                        key={model.id + key}
-                                        className={`min-w-[calc(25%+0.5rem)] w-full flex pr-4 py-6 ${
-                                            index === 0 ? 'border-t-[1px]' : ''
-                                        } border-b-[1px] border-secondary/35`}
-                                    >
-                                        <div className='text-secondary capitalize font-medium'>
-                                            {renderValue(key, model[key])}
-                                        </div>
-                                    </div>
-                                ))}
-                            </div>
-                        ))}
-                    </div>
+                            ))}
+                        </div>
+                    ))}
                 </div>
             </div>
         </main>
--- src/Components/CompareModels.tsx
+++ src/Components/CompareModels.tsx
@@ -1,6 +1,23 @@
 import CompareCard from './CompareCard';
+import { useDispatch, useSelector } from 'react-redux';
+import { replaceModel, toggleShowDifferences } from '../store/compareSlice';
+import type { RootState } from '../store/store';
 
-export default function CompareModels({ models }: { models: { id: number, name: string, pic: string }[]}) {
+export default function CompareModels({
+    models,
+}: {
+    models: { id: number; name: string; pic: string }[];
+}) {
+    const dispatch = useDispatch();
+    const showDifferences = useSelector((state: RootState) => state.compare.showDifferences);
+
+    const selectedModels = useSelector(
+        (state: RootState) => state.compare.selectedModels
+    );
+
+    // console.log('models', selectedModels);
+
+    console.log(showDifferences);
 
     return (
         <header className='flex flex-col py-12 px-2'>
@@ -12,14 +29,23 @@ export default function CompareModels({ models }: { models: { id: number, name: 
                                 type='checkbox'
                                 id='showDifferences'
                                 className='mr-1.5'
+                                checked={showDifferences}
+                                onChange={() => dispatch(toggleShowDifferences())}
                             />
                             Показать различия
                         </label>
                     </div>
 
-                    {models.map((model) => {
+                    {models.map((model, index) => {
                         return (
-                            <CompareCard model={model} key={model.id} />
+                            <CompareCard
+                                model={model}
+                                key={model.id}
+                                onReplace={(newModel) =>
+                                    dispatch(replaceModel({ index, newModel }))
+                                }
+                                selectedModels={selectedModels}
+                            />
                         );
                     })}
                 </div>
