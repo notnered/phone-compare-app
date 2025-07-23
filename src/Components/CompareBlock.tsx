1c8096edad1033694bfcb4a48ec792cfbe8989d3
Konstantin Nered <konstantin.n616@gmail.com>
2025-07-24 00:06:19 +0700

    updated filter

--- src/Components/CompareBlock.tsx
+++ src/Components/CompareBlock.tsx
@@ -1,16 +1,20 @@
-import { useState } from "react";
-import CompareFilters from "./CompareFilters";
-import CompareMain from "./CompareMain";
+import { useState } from 'react';
+import CompareFilters from './CompareFilters';
+import CompareMain from './CompareMain';
 import { models } from '../data/phoneModels';
 
-export default function CompareBlock(){
-    const [compareLimit, setCompareLimit] = useState(3);
-    const allModelsLength = models.length;
+export default function CompareBlock() {
+    const [compareLimit, setCompareLimit] = useState<number>(3);
+    const allModelsLength = models.length > 6 ? 6 : models.length;
 
     return (
         <>
-            <CompareFilters limit={compareLimit} limitFunc={setCompareLimit} len={allModelsLength} />
+            <CompareFilters
+                limit={compareLimit}
+                limitFunc={setCompareLimit}
+                len={allModelsLength}
+            />
             <CompareMain limit={compareLimit} />
         </>
-    )
+    );
 }
--- src/Components/CompareFilters.tsx
+++ src/Components/CompareFilters.tsx
@@ -1,6 +1,9 @@
 export default function CompareFilters({ limit, limitFunc, len }: { limit: number, limitFunc: React.Dispatch<React.SetStateAction<number>>, len: number }) {
+    const limitArray = [];
 
-    const limitArray = Array.from({ length: len }, (_, i) => i + 1);
+    for(let i = 2; i <= len; i++){
+        limitArray.push(i);
+    }
 
     return (
         <div className='wrapper px-2 pt-16 w-full flex items-center justify-between'>
@@ -11,13 +14,6 @@ export default function CompareFilters({ limit, limitFunc, len }: { limit: numbe
             </div>
             <div className='flex gap-x-1.5 text-primary'>
                 Отобразить товары:
-                {/* <div className='hover:cursor-pointer'>2</div>
-                <div className='hover:cursor-pointer underline underline-offset-4'>
-                    3
-                </div>
-                <div className='hover:cursor-pointer'>4</div>
-                <div className='hover:cursor-pointer'>5</div>
-                <div className='hover:cursor-pointer'>6</div> */}
                 {limitArray.map((number, index) => {
                     return (
                         <div className={`hover:cursor-pointer ${number === limit ? 'underline underline-offset-4' : ''}`} key={index} onClick={() => limitFunc(number)}>{number}</div>
